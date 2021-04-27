0021394e86669c7f2afb0875e8559cc55a387e4d
Robert Schade <roscha80@web.de>
2021-04-27 15:25:35 +0200

    Delete margin in storybook, add hover to button.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,7 @@ import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
 import '../src/styles/styles.css'
 
 export const parameters = {
+  layout: 'fullscreen',
   actions: { argTypesRegex: '^on[A-Z].*' },
   viewport: {
     viewports: INITIAL_VIEWPORTS,
