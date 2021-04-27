af87cfd5ad1a9d5ea0e95e28695cbab0362de5cd
Robert Schade <roscha80@web.de>
2021-04-27 16:25:38 +0200

    Add bookmark toggle to quiz-card.html and index.html

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 const answer = document.querySelector('.answer')
 const buttonShow = document.querySelector('.buttonShowAnswer')
 
+const bookmark = document.querySelector('.quiz-card__bookmark')
+const bookmarkAdded = document.querySelector('.quiz-card__bookmark--added')
+
 buttonShow.addEventListener('click', () => {
   answer.classList.toggle('hidden')
+})
+
+bookmark.addEventListener('click', () => {
+  bookmarkAdded.classList.toggle('quiz-card__bookmark--added')
 })
