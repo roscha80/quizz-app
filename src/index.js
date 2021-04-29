523558dbbf6895e777cae3e345fb6996d990b2bc
Robert Schade <roscha80@web.de>
2021-04-29 15:35:12 +0200

    Clean js functinality

--- src/index.js
+++ src/index.js
@@ -1,81 +1,104 @@
-const answer = document.querySelector('.answer')
-const buttonShow = document.querySelector('.buttonShowAnswer')
+const answer = getElement('.answer')
+const buttonShow = getElement('.buttonShowAnswer')
 
-const bookmark = document.querySelector('.js__bookmark')
-const bookmarkAdded = document.querySelector('.js__bookmark--added')
+const bookmark = getElement('.js__bookmark')
+const bookmarkAdded = getElement('.js__bookmark--added')
 
-const homeScreen = document.querySelector('[data-page="js__home"]')
-const createScreen = document.querySelector('[data-page="js__form"]')
-const bookmarkedScreen = document.querySelector('[data-page="js__bookmarked"]')
-const profileScreen = document.querySelector('[data-page="js__profile"]')
+const homeScreen = getElement('[data-page="js__home"]')
+const createScreen = getElement('[data-page="js__form"]')
+const bookmarkedScreen = getElement('[data-page="js__bookmarked"]')
+const profileScreen = getElement('[data-page="js__profile"]')
 
-const homeButtonShow = document.querySelector('[data-button="js__button-home"]')
-const createButtonShow = document.querySelector(
-  '[data-button="js__button-create"]'
-)
-const bookmarkedButtonShow = document.querySelector(
-  '[data-button="js__button-bookmarked"]'
-)
-const profileButtonShow = document.querySelector(
-  '[data-button="js__button-profile"]'
-)
+const homeButtonShow = getElement('[data-button="js__button-home"]')
+const createButtonShow = getElement('[data-button="js__button-create"]')
+const bookmarkedButtonShow = getElement('[data-button="js__button-bookmarked"]')
+const profileButtonShow = getElement('[data-button="js__button-profile"]')
 
-const header = document.querySelector('[data-header]')
+const header = getElement('[data-header]')
 
-buttonShow.addEventListener('click', () => {
-  answer.classList.toggle('hidden')
-})
+buttonShow.addEventListener('click', hideAnswer)
 
-bookmark.addEventListener('click', () => {
-  bookmarkAdded.classList.toggle('quiz-card__bookmark--added')
-})
+bookmark.addEventListener('click', changeBookmark)
 
-const hideAllPages = () => {
+homeButtonShow.addEventListener('click', navigateToHome)
+
+createButtonShow.addEventListener('click', navigateToCreate)
+
+bookmarkedButtonShow.addEventListener('click', navigateToBookmarked)
+
+profileButtonShow.addEventListener('click', navigateToProfile)
+
+function hideAllPages() {
   homeScreen.classList.add('hidden')
   createScreen.classList.add('hidden')
   bookmarkedScreen.classList.add('hidden')
   profileScreen.classList.add('hidden')
 }
 
-const unactiveAllButtons = () => {
+function deactivateAllButtons() {
   homeButtonShow.classList.remove('navbar__button-active')
   createButtonShow.classList.remove('navbar__button-active')
   bookmarkedButtonShow.classList.remove('navbar__button-active')
   profileButtonShow.classList.remove('navbar__button-active')
 }
 
-homeButtonShow.addEventListener('click', () => {
+function getElement(selector) {
+  return document.querySelector(selector)
+}
+
+function navigateToHome() {
+  changeScreen(homeScreen)
+  activateButton(homeButtonShow)
+  changeHeaderTextHome()
+}
+function navigateToCreate() {
+  changeScreen(createScreen)
+  activateButton(createButtonShow)
+  changeHeaderTextCreate()
+}
+
+function navigateToBookmarked() {
+  changeScreen(bookmarkedScreen)
+  activateButton(bookmarkedButtonShow)
+  changeHeaderTextBookmarked()
+}
+
+function navigateToProfile() {
+  changeScreen(profileScreen)
+  activateButton(profileButtonShow)
+  changeHeaderTextProfile()
+}
+
+function changeHeaderTextHome() {
   header.innerText = 'Magic Quizard'
-  hideAllPages()
-  homeScreen.classList.remove('hidden')
+}
 
-  unactiveAllButtons()
-  homeButtonShow.classList.add('navbar__button-active')
-})
-
-createButtonShow.addEventListener('click', () => {
+function changeHeaderTextCreate() {
   header.innerText = 'Create'
-  hideAllPages()
-  createScreen.classList.remove('hidden')
+}
 
-  unactiveAllButtons()
-  createButtonShow.classList.add('navbar__button-active')
-})
-
-bookmarkedButtonShow.addEventListener('click', () => {
+function changeHeaderTextBookmarked() {
   header.innerText = 'Bookmarked'
-  hideAllPages()
-  bookmarkedScreen.classList.remove('hidden')
+}
 
-  unactiveAllButtons()
-  bookmarkedButtonShow.classList.add('navbar__button-active')
-})
-
-profileButtonShow.addEventListener('click', () => {
+function changeHeaderTextProfile() {
   header.innerText = 'Profile'
-  hideAllPages()
-  profileScreen.classList.remove('hidden')
+}
 
-  unactiveAllButtons()
-  profileButtonShow.classList.add('navbar__button-active')
-})
+function changeScreen(page) {
+  hideAllPages()
+  page.classList.remove('hidden')
+}
+
+function activateButton(button) {
+  deactivateAllButtons()
+  button.classList.add('navbar__button-active')
+}
+
+function hideAnswer() {
+  answer.classList.toggle('hidden')
+}
+
+function changeBookmark() {
+  bookmarkAdded.classList.toggle('quiz-card__bookmark--added')
+}
