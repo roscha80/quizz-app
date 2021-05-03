18639d20c034de21573b7b39f19428892608a0b1
Robert Schade <roscha80@web.de>
2021-05-03 15:42:38 +0200

    refactoring js, add dynamic header text

--- src/setupNavigation.js
+++ src/setupNavigation.js
@@ -1,24 +1,40 @@
 import getAllElements from './utils/getAllElements'
+import getElement from './utils/getElement'
 
 export default function setupNavigation() {
   const pages = getAllElements('[data-page]')
   const buttons = getAllElements('[data-nav]')
+  const header = getElement('[data-header]')
 
-  buttons.forEach(button => {
+  buttons.forEach(setupButton)
+
+  function setupButton(button) {
     const buttonName = button.dataset.nav
-
+    const headerText = button.dataset.headerText
     button.addEventListener('click', () => {
-      pages.forEach(page => {
-        const pageName = page.dataset.page
-
-        page.classList.toggle('hidden', pageName !== buttonName)
-      })
-      buttons.forEach(buttonActive => {
-        buttonActive.classList.toggle(
-          'navbar__button-active',
-          buttonName === buttonActive.dataset.nav
-        )
-      })
+      updateHeader(headerText)
+      changePage(buttonName)
+      updateNavigation(buttonName)
     })
-  })
+  }
+
+  function updateHeader(text) {
+    header.innerText = text
+  }
+
+  function updateNavigation(buttonName) {
+    buttons.forEach(buttonActive => {
+      buttonActive.classList.toggle(
+        'navbar__button-active',
+        buttonName === buttonActive.dataset.nav
+      )
+    })
+  }
+
+  function changePage(buttonName) {
+    pages.forEach(page => {
+      const pageName = page.dataset.page
+      page.classList.toggle('hidden', pageName !== buttonName)
+    })
+  }
 }
